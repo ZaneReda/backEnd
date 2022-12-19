8f581fec6f30ae13961ab9dea6655f9ec1435495
ZaneReda <59884549+ZaneReda@users.noreply.github.com>
2022-12-19 22:48:36 +0100

    fvggr

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 load_dotenv()
 
 class Config(object):
-    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SECRET_KEY = 'empty'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class ProductionConfig(Config):
