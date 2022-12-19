f20193d6d771d50bb5f634aacb3ebce86888515e
ZaneReda <59884549+ZaneReda@users.noreply.github.com>
2022-12-19 02:13:54 +0100

    pytest problems setup 3

--- tests/functional/test_routes.py
+++ tests/functional/test_routes.py
@@ -12,15 +12,25 @@ def test_dummy_wrong_route():
         assert response.status_code == 404
 
 def test_create_recipe(testing_client):
-        response = testing_client.post('/')
-        assert response.status_code == 200
+        response = testing_client.post('/', json={'name': 'test', 'ingredients': 'test', 'instructions': 'test', 'favorite': False, 'rating': 0})
+        assert response.json['name'] == 'test'
+        assert response.json['ingredients'] == 'test'
+        assert response.json['instructions'] == 'test'
+        assert response.json['favorite'] == False
+        assert response.json['rating'] == 0
+        assert response.status_code == 201
 
 def test_update_recipe(testing_client):
-        response = testing_client.put('/')
+        response = testing_client.put('/1', json={'name': 'test', 'ingredients': 'test', 'instructions': 'test', 'favorite': False, 'rating': 0})
+        assert response.json['name'] == 'test'
+        assert response.json['ingredients'] == 'test'
+        assert response.json['instructions'] == 'test'
+        assert response.json['favorite'] == False
+        assert response.json['rating'] == 0
         assert response.status_code == 200
 
 def test_delete_recipe(testing_client):
-        response = testing_client.delete('/')
+        response = testing_client.delete('/1')
         assert response.status_code == 200
 
 def test_get_recipes(testing_client):
