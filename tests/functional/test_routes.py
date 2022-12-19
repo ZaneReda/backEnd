2fb2ae5646a908df41159f34a3a78077a5fc35e5
ZaneReda <59884549+ZaneReda@users.noreply.github.com>
2022-12-19 01:41:25 +0100

    pytest problems setup 2

--- tests/functional/test_routes.py
+++ tests/functional/test_routes.py
@@ -2,29 +2,28 @@ from backend_api import app
 import pytest
 
 def test_get_recipe(testing_client):
-    response = testing_client.get('/darecipes')
-    assert response.status_code == 200
-
+        response = testing_client.get('/1')
+        assert response.status_code == 200
+    
 
 def test_dummy_wrong_route(): 
-
     with app.test_client() as client:
         response = client.get('/wrong_route')
         assert response.status_code == 404
 
 def test_create_recipe(testing_client):
-    response = testing_client.post('/darecipes')
-    assert response.status_code == 201
+        response = testing_client.post('/')
+        assert response.status_code == 200
 
 def test_update_recipe(testing_client):
-    response = testing_client.put('/darecipes')
-    assert response.status_code == 200
+        response = testing_client.put('/')
+        assert response.status_code == 200
 
 def test_delete_recipe(testing_client):
-    response = testing_client.delete('/darecipes')
-    assert response.status_code == 200
+        response = testing_client.delete('/')
+        assert response.status_code == 200
 
 def test_get_recipes(testing_client):
-    response = testing_client.get('/darecipes')
-    assert response.status_code == 200
+        response = testing_client.get('/')
+        assert response.status_code == 200
 
